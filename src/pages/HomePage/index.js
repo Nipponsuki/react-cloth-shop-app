449d7a5a036854e58cd59ee08cb4b5032ccd331a
Alexey Alexeev <alex.alexeev23061991@gmail.com>
2020-01-10 19:33:59 +0300

    project setup

---
+++ src/pages/HomePage/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+import "./styles.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATs</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATs</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATs</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATs</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATs</h1>
+          <span className="subtitle">shop now</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
